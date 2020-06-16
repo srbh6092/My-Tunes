bb248142fd0a8f96c2d910097d245baa9451bfd9
Saurabh Singh <saurabhgkp20@gmail.com>
2020-06-16 14:34:43 +0530

    Initial commit(Reading music files).

---
+++ app/src/main/java/com/saurabh/mytunes/AlbumsFragment.java
@@ -0,0 +1,23 @@
+package com.saurabh.mytunes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AlbumsFragment extends Fragment {
+
+    public AlbumsFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_albums, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/saurabh/mytunes/SongsFragment.java
@@ -0,0 +1,24 @@
+package com.saurabh.mytunes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SongsFragment extends Fragment {
+
+    public SongsFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_songs, container, false);
+
+        return view;
+    }
+}
