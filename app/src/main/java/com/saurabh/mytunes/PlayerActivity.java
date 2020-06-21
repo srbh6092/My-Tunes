1be056f42368c4447503fd9f6137b02046eab7ab
Saurabh Singh <saurabhgkp20@gmail.com>
2020-06-21 16:49:52 +0530

    Play previous, next songs. Set background and gradient with respect to the album art.

--- app/src/main/java/com/saurabh/mytunes/PlayerActivity.java
+++ app/src/main/java/com/saurabh/mytunes/PlayerActivity.java
@@ -1,15 +1,24 @@
 package com.saurabh.mytunes;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.palette.graphics.Palette;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -25,12 +34,13 @@ public class PlayerActivity extends AppCompatActivity {
     FloatingActionButton mPlay;
     SeekBar mSeekBar;
     int position;
-    Uri uri;
 
     static ArrayList<MusicFiles> listSongs = new ArrayList<>();
     static MediaPlayer mediaPlayer;
+    static Uri uri;
 
-    Handler handler = new Handler();
+    private Handler handler = new Handler();
+    private Thread prevThread, playThread, nextThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,19 +88,85 @@ public class PlayerActivity extends AppCompatActivity {
 
             }
         });
-        PlayerActivity.this.runOnUiThread(new Runnable() {
+        setDurationSeekBar();
+
+    }
+
+    @Override
+    protected void onResume() {
+        prevThreadBtn();
+        playThreadBtn();
+        nextThreadBtn();
+        super.onResume();
+    }
+
+    private void prevThreadBtn() {
+        prevThread = new Thread(){
             @Override
             public void run() {
-                if(mediaPlayer!=null)
-                {
-                    int mCurrentPosition = mediaPlayer.getCurrentPosition()/1000;
-                    mSeekBar.setProgress(mCurrentPosition);
-                    mDurationPlayed.setText(formattedTime(mCurrentPosition));
-                }
-                handler.postDelayed(this,1000);
+                super.run();
+                mPrevious.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        prevBtnClicked();
+                    }
+                });
             }
-        });
+        };
+        prevThread.start();;
+    }
 
+    private void prevBtnClicked() {
+        position=(position-1)<0?listSongs.size()-1:position-1;
+        startSongAtPosition();
+    }
+
+    private void playThreadBtn() {
+        playThread = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                mPlay.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        playPauseBtnClicked();
+                    }
+                });
+            }
+        };
+        playThread.start();;
+    }
+
+    private void playPauseBtnClicked() {
+        if(mediaPlayer.isPlaying()) {
+            mPlay.setImageResource(R.drawable.ic_play);
+            mediaPlayer.pause();
+        } else {
+            mPlay.setImageResource(R.drawable.ic_pause);
+            mediaPlayer.start();
+        }
+        setDurationSeekBar();
+    }
+
+    private void nextThreadBtn() {
+        nextThread = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                mNext.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        nextBtnClicked();
+                    }
+                });
+            }
+        };
+        nextThread.start();;
+    }
+
+    private void nextBtnClicked() {
+        position=((position+1)%listSongs.size());
+        startSongAtPosition();
     }
 
     private String formattedTime(int mCurrentPosition) {
@@ -132,12 +208,73 @@ public class PlayerActivity extends AppCompatActivity {
             int durationTotal = Integer.parseInt(listSongs.get(position).getDuration())/1000;
             mDurationTotal.setText(formattedTime(durationTotal));
             art = retriever.getEmbeddedPicture();
+            Bitmap bitmap;
             retriever.release();
-            if(art!=null)
+            if(art!=null){
                 Glide.with(this).asBitmap().load(art).into(mAlbumArt);
+                bitmap = BitmapFactory.decodeByteArray(art,0,art.length);
+                Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(@Nullable Palette palette) {
+                        Palette.Swatch swatch = palette.getDominantSwatch();
+                        ImageView mGradient = findViewById(R.id.gradient);
+                        LinearLayout mContainer = findViewById(R.id.linearLayout);
+                        if(swatch!=null)
+                        {
+                            GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP,new int[]{swatch.getRgb(),0x00000000});
+                            mGradient.setBackground(gradientDrawable);
+                            GradientDrawable gradientDrawableBg = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP,new int[]{swatch.getRgb(),swatch.getRgb()});
+                            mContainer.setBackground(gradientDrawableBg);
+                            mSongName.setTextColor(swatch.getTitleTextColor());
+                            mSongArtist.setTextColor(swatch.getBodyTextColor());
+                        }
+                        else
+                        {
+                            GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP,new int[]{0xff000000,0x00000000});
+                            mGradient.setBackground(gradientDrawable);
+                            GradientDrawable gradientDrawableBg = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP,new int[]{0xff000000,0xff000000});
+                            mContainer.setBackground(gradientDrawableBg);
+                            mSongName.setTextColor(Color.RED);
+                            mSongArtist.setTextColor(Color.BLUE);
+                        }
+                    }
+                });
+            }
         }catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void startSongAtPosition() {
+        if(mediaPlayer.isPlaying())
+        {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+        mPlay.setImageResource(R.drawable.ic_pause);
+        uri=Uri.parse(listSongs.get(position).getPath());
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),uri);
+        metaData(uri);
+        mSongName.setText(listSongs.get(position).getTitle());
+        mSongArtist.setText(listSongs.get(position).getArtist());
+        setDurationSeekBar();
+        mediaPlayer.start();
+    }
+
+    private void setDurationSeekBar() {
+        mSeekBar.setMax(mediaPlayer.getDuration()/1000);
+        PlayerActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(mediaPlayer!=null)
+                {
+                    int mCurrentPosition = mediaPlayer.getCurrentPosition()/1000;
+                    mSeekBar.setProgress(mCurrentPosition);
+                    mDurationPlayed.setText(formattedTime(mCurrentPosition));
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
     }
 
 }
