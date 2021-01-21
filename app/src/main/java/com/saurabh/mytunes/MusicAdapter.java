b023d609966455706086bbf1d6f4fbf5ae5b1676
Saurabh Singh <saurabhgkp20@gmail.com>
2021-01-21 19:55:56 +0530

    Change: Adding delete option to song items.

--- app/src/main/java/com/saurabh/mytunes/MusicAdapter.java
+++ app/src/main/java/com/saurabh/mytunes/MusicAdapter.java
@@ -1,16 +1,25 @@
 package com.saurabh.mytunes;
 
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
@@ -49,6 +58,42 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MyViewHolder
                 mContext.startActivity(intent);
             }
         });
+        holder.menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                PopupMenu popupMenu = new PopupMenu(mContext,view);
+                popupMenu.getMenuInflater().inflate(R.menu.pop_up,popupMenu.getMenu());
+                popupMenu.show();
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.delete:
+                                Toast.makeText(mContext,"Deleted",Toast.LENGTH_SHORT).show();
+                                deleteSong(position,view);
+                        }
+                        return true;
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void deleteSong(int position,View v){
+        Uri contentUri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,Long.parseLong(mFiles.get(position).getId()));
+        mFiles.remove(position);
+        File file = new File(mFiles.get(position).getPath());
+        boolean isDeleted = file.delete();
+        if(isDeleted){
+            mContext.getContentResolver().delete(contentUri,null,null);
+            mFiles.remove(position);
+            notifyItemRemoved(position);
+            notifyItemRangeChanged(position,mFiles.size());
+            Snackbar.make(v,"File Deleted", BaseTransientBottomBar.LENGTH_SHORT).show();
+        }
+        else
+            Snackbar.make(v,"File couldn't be deleted from the Internal Storage", BaseTransientBottomBar.LENGTH_SHORT).show();
     }
 
     @Override
@@ -60,11 +105,13 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MyViewHolder
 
         TextView fileName;
         ImageView albumArt;
+        ImageView menu;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             fileName = itemView.findViewById(R.id.fileNameMusic);
             albumArt = itemView.findViewById(R.id.imgMusic);
+            menu = itemView.findViewById(R.id.menu);
         }
     }
 
